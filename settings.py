805413a4df6a88a5755a8760c550ae0a7f2819d0
翟持江 <129171955+2513502304@users.noreply.github.com>
2025-04-06 13:43:40 +0800

    Update settings.py，添加了fix_ratio的过滤选项

--- settings.py
+++ settings.py
@@ -1,13 +1,18 @@
 from typing import Literal
 
 # 存储作为 block 区域图像的文件夹路径
-image_dir: str = './Data/K-ON!'
+image_dir: str = './Data/K-ON!'  # r'D:\YandereSpider\downloads\posts\k-on!'
 
 # block 区域图像的高
-block_height: int = 9 * 6
+block_height: int = 9 * 6  # 9 * 30
 
 # block 区域图像的宽
-block_width: int = 16 * 6
+block_width: int = 16 * 61  # 6 * 30
+
+# 在作为 block 区域图像的文件中，是否仅保留原图比例与 block_height / block_width 比例一致的 block 区域图像
+# 若为 True，则会应用满足与 block_height / block_width 比例一致的 block 区域图像，忽略其他比例的图像
+# 若为 False，则会应用所有的 block 区域图像
+fix_ratio: bool = True
 
 # 是否懒加载 block 区域图像，若为 False，则一次性缓存所有的 block 区域图像至内存中，该设置可显著加快程序运行速度，但不适用于 block 区域图像过多的场景。强烈建议图像数量不超过 1w 张以上时均设置为 False
 lazy_load: bool = False
@@ -19,13 +24,13 @@ scale: int | float = 3.0
 fit_size: bool = True
 
 # 是否限制每个 block 区域图像仅出现一次
-unique: bool = False
+unique: bool = False  # True
 
 # 蒙太奇图像输入路径
 image_input_path: str = './Data/input/Yui-Azusa.jpg'
 
 # 蒙太奇图像输出路径
-image_output_path: str = './Data/output/Yui-Azusa.jpg'
+image_output_path: str = './Data/output/Yui-Azusa.jpg'  # './Data/output/Unique-Yui-Azusa.jpg'
 
 # 保存训练好的蒙太奇对象路径
 # 若提供该路径，则优先加载该路径下的蒙太奇对象，并判断是否该对象参数是否与当前配置参数是否一致，若一致则直接使用该对象
