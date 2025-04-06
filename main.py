51c62d41adbfaac80d3d4556b38d02d2d9a92fee
翟持江 <129171955+2513502304@users.noreply.github.com>
2025-04-06 13:40:54 +0800

    Update main.py，添加了fix_ratio的过滤选项

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Author: 未来可欺 2513502304@qq.com
 Date: 2025-03-22 12:31:06
 LastEditors: 未来可欺 2513502304@qq.com
-LastEditTime: 2025-03-26 20:00:22
+LastEditTime: 2025-04-03 12:59:43
 Description: 应用于视频或图像的蒙太奇生成工具
 """
 
@@ -14,12 +14,12 @@ import os
 def main():
     if os.path.exists(settings.pkl_path):
         montage: Montage = joblib.load(settings.pkl_path)
-        different = montage.image_dir != settings.image_dir or montage.block_height != settings.block_height or montage.block_width != settings.block_width or montage.lazy_load != settings.lazy_load
+        different = montage.image_dir != settings.image_dir or montage.block_height != settings.block_height or montage.block_width != settings.block_width or montage.fix_ratio != settings.fix_ratio or montage.lazy_load != settings.lazy_load
         if different:
-            montage = Montage(image_dir=settings.image_dir, block_height=settings.block_height, block_width=settings.block_width, lazy_load=settings.lazy_load)
+            montage: Montage = Montage(image_dir=settings.image_dir, block_height=settings.block_height, block_width=settings.block_width, fix_ratio=settings.fix_ratio, lazy_load=settings.lazy_load)
             joblib.dump(montage, settings.pkl_path)
     else:
-        montage: Montage = Montage(image_dir=settings.image_dir, block_height=settings.block_height, block_width=settings.block_width, lazy_load=settings.lazy_load)
+        montage: Montage = Montage(image_dir=settings.image_dir, block_height=settings.block_height, block_width=settings.block_width, fix_ratio=settings.fix_ratio, lazy_load=settings.lazy_load)
         joblib.dump(montage, settings.pkl_path)
     match settings.mode.lower():
         case 'image':
